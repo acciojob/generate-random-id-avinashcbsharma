36d4462964a50bffb6888bf45884d2b5453c623b
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-14 13:38:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function makeid(l) {
-  // write your code here
+   const Charac = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwZxYyZz0123456789!@#$%^&*()+~`=-'
+    let ranno = 0;
+    str='';
+    resStr='';
+    for(let i=0; i<l ; i++){
+		
+        ranno=Math.floor(Math.random()* 100);
+        if(ranno < 0 || ranno > Charac.length){
+            i--;
+        }else{
+            str= Charac.slice(ranno,ranno+1);
+            resStr = resStr + str
+        }
+    }
+    return resStr;
 }
 
 // Do not change the code below.
